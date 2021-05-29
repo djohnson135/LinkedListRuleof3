12541c203b7c80f238c77d89f644774ed4e7688c
djohnson135 <dathanljohnson135@gmail.com>
2021-05-29 17:53:56 -0500

    destructor

--- linkedlist.h
+++ linkedlist.h
@@ -8,12 +8,43 @@ class linkedlist{
     node* tail;
     node* head;
     public:
+    linkedlist() : head(nullptr), tail(nullptr), size(0) {} //default constructor
 
-    linkedlist() : head(nullptr), tail(nullptr), size(0) {}
     linkedlist(const linkedlist & src); //copy constructor
-    ~linkedlist(); //delete all saved memory from the heap
-    //create something that initiliazes the list
-    void pushlist();
+    ~linkedlist(); //delete all saved memory from the heap this is the destructor
+    linkedlist & operator=(const linkedlist & src); //copy assignment operator
+    unsigned int returnsize() const {return size;}
+    int frontdata() const {return head->data;}
+    int backdata() const {return tail->data;}
+    void push_back(int);
+    void push_front(int);
+    void pop_front(int);
+    void pop_back(int);
+    void erase(unsigned int indx);
+    void insert(int data, unsigned int indx);
+    void clear();
+
 };
 
+linkedlist::~linkedlist(){ //destrucor
+    //delete all memory stored on the heap
+    node* delnode = nullptr;
+    while (head != nullptr){
+        delnode = head;
+        head = head->next;
+        delete delnode;
+    }
+    head = nullptr;
+    tail = nullptr;
+    size = 0;
+}
+    
+
+linkedlist::linkedlist(const linkedlist & src){
+
+}
+
+linkedlist & linkedlist::operator=(const linkedlist & src){
+    return *this;
+}
 #endif
--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include "linkedlist.h"
 
-
+//this is going to be a doubly linked list and not the typical one
+// I used template typename in the creation of this list
 
 int main(){
-    node linkedlist;
-    linkedlist.nodeinit();
-
-
+    linkedlist ll;
+    linkedlist ff;
+    //test
+    //copy assingment operator test
+    ll = ff;
+    //copy constructor test
+    linkedlist gg(ff);
 
 }
